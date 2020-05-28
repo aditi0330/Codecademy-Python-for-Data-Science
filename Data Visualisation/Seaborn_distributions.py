485625e8536c8d03d6c1b73834bac00a4fd318eb
Aditi Shukla <42002395+aditi0330@users.noreply.github.com>
2020-05-28 23:50:17 +0530

    KDE Plots

---
+++ Data Visualisation/Seaborn_distributions.py
@@ -0,0 +1,65 @@
+#Bar Charts Hide Information
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# Take in the data from the CSVs as NumPy arrays:
+set_one = np.genfromtxt("dataset1.csv", delimiter=",")
+set_two = np.genfromtxt("dataset2.csv", delimiter=",")
+set_three = np.genfromtxt("dataset3.csv", delimiter=",")
+set_four = np.genfromtxt("dataset4.csv", delimiter=",")
+
+# Creating a Pandas DataFrame:
+n=500
+df = pd.DataFrame({
+    "label": ["set_one"] * n + ["set_two"] * n + ["set_three"] * n + ["set_four"] * n,
+    "value": np.concatenate([set_one, set_two, set_three, set_four])
+})
+
+# Setting styles:
+sns.set_style("darkgrid")
+sns.set_palette("pastel")
+
+# Add your code below:
+sns.barplot(data=df, x='label', y='value')
+plt.show()
+
+#KDE Plots
+#Seaborn offers another option for graphing distributions: KDE Plots.
+#KDE stands for Kernel Density Estimator. A KDE plot gives us the sense of a univariate as a curve.
+# A univariate dataset only has one variable and is also referred to as being one-dimensional, as opposed to bivariate or two-dimensional datasets which have two variables.
+#KDE plots are preferable to histograms because depending on how you group the data into bins and the width of the bins,
+#you can draw wildly different conclusions about the shape of the data.
+#Using a KDE plot can mitigate these issues, because they smooth the datasets, allow us to generalize over the shape of our data, and aren’t beholden to specific data points.
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# Take in the data from the CSVs as NumPy arrays:
+set_one = np.genfromtxt("dataset1.csv", delimiter=",")
+set_two = np.genfromtxt("dataset2.csv", delimiter=",")
+set_three = np.genfromtxt("dataset3.csv", delimiter=",")
+set_four = np.genfromtxt("dataset4.csv", delimiter=",")
+
+# Creating a Pandas DataFrame:
+n=500
+df = pd.DataFrame({
+    "label": ["set_one"] * n + ["set_two"] * n + ["set_three"] * n + ["set_four"] * n,
+    "value": np.concatenate([set_one, set_two, set_three, set_four])
+})
+
+# Setting styles:
+sns.set_style("darkgrid")
+sns.set_palette("pastel")
+
+# Add your code below:
+sns.kdeplot(set_one, shade=True)
+sns.kdeplot(set_two, shade=True)
+sns.kdeplot(set_three, shade=True)
+sns.kdeplot(set_four, shade=True)
+
+plt.show()
+
