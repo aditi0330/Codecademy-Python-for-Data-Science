416d793937dbb58fc6436c9a1bbf933bf98948d0
Aditi Shukla <42002395+aditi0330@users.noreply.github.com>
2020-06-04 23:50:38 +0530

    Project

--- Statistics/Life_expectancy_by_Country.py
+++ Statistics/Life_expectancy_by_Country.py
@@ -1,4 +1,4 @@
-import codecademylib3_seaborn
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,5 +10,14 @@ life_expectancy_quartiles = np.quantile(life_expectancy, [0.25, 0.50, 0.75])
 gdp = data['GDP']
 median_gdp = np.quantile(gdp, 0.5)
 print(median_gdp)
-plt.hist(life_expectancy)
+
+low_gdp = data[data['GDP'] <= median_gdp]
+high_gdp = data[data['GDP'] >= median_gdp]
+
+low_gdp_quartiles = np.quantile(low_gdp["Life Expectancy"], [0.25, 0.50, 0.75])
+high_gdp_quartiles = np.quantile(high_gdp["Life Expectancy"], [0.25, 0.50, 0.75])
+
+plt.hist(high_gdp["Life Expectancy"], alpha = 0.5, label = "High GDP")
+plt.hist(low_gdp["Life Expectancy"], alpha = 0.5, label = "Low GDP")
+plt.legend()
 plt.show()
