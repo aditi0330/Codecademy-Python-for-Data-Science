0ee48691c3f5e78bef5ab80412f8a5e0d14a1f52
Aditi Shukla <42002395+aditi0330@users.noreply.github.com>
2020-06-05 16:06:09 +0530

    Project

---
+++ Statistics/Healthcare_in_different_states.py
@@ -0,0 +1,22 @@
+import codecademylib3_seaborn
+import pandas as pd
+from matplotlib import pyplot as plt
+
+healthcare = pd.read_csv("healthcare.csv")
+print(healthcare.head())
+print(healthcare["DRG Definition"].unique())
+
+chest_pain = healthcare[healthcare['DRG Definition'] == '313 - CHEST PAIN']
+ny_chest_pain = chest_pain[chest_pain['Provider State'] == "NY"]
+costs = ny_chest_pain[' Average Covered Charges '].values
+#plt.boxplot(costs)
+#plt.show()
+
+states = chest_pain["Provider State"].unique()
+datasets = []
+for state in states:
+  datasets.append(chest_pain[chest_pain['Provider State'] == state][' Average Covered Charges '].values)
+
+plt.figure(figsize=(20,6))
+plt.boxplot(datasets, labels = states)
+plt.show()
