c8310841ff755d8844b6ec3dc16c5860d737069e
Aditi Shukla <42002395+aditi0330@users.noreply.github.com>
2020-06-09 23:48:14 +0530

    Sample Testing

--- Scipy/Hypothesis_Testing_2.py
+++ Scipy/Hypothesis_Testing_2.py
@@ -87,3 +87,19 @@ not_normal = 4
 ratio = np.std(dist_2)/np.std(dist_3)
 
 #Tukey's Range Test
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
+a = np.genfromtxt("store_a.csv",  delimiter=",")
+b = np.genfromtxt("store_b.csv",  delimiter=",")
+c = np.genfromtxt("store_c.csv",  delimiter=",")
+
+stat, pval = f_oneway(a, b, c)
+print pval
+
+# Using our data from ANOVA, we create v and l
+v = np.concatenate([a, b, c])
+labels = ['a'] * len(a) + ['b'] * len(b) + ['c'] * len(c)
+tukey_results = pairwise_tukeyhsd(v, labels, 0.05)
+print(tukey_results)
+
+#Binomial Test
