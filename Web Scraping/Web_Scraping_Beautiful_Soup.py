92c59865fba64da8161a95fc87b3804433ece777
Aditi Shukla <42002395+aditi0330@users.noreply.github.com>
2020-06-15 23:37:48 +0530

    Update Web Scraping

--- Web Scraping/Web_Scraping_Beautiful_Soup.py
+++ Web Scraping/Web_Scraping_Beautiful_Soup.py
@@ -13,3 +13,18 @@ webpage = webpage_response.content
 soup = BeautifulSoup(webpage, "html.parser")
 
 print(soup)
+#to print the first p tag
+print(soup.p)
+#to print string associated with the first p tag
+print(soup.p.string)
+
+for child in soup.ul.children:
+    print(child)
+    
+for parent in soup.li.parents:
+    print(parent)
+#Above two loos print out the same thing.
+
+#Loop through all of the children of the first div and print out each one.
+for child in soup.div.children:
+  print(child)
